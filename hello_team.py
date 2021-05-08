83b7e1e7553a43cf12057b9991d0df80cd2ff2db
kefei <kefeimo@gmail.com>
2021-05-08 12:15:46 -0700

    Initial commit

---
+++ hello_team.py
@@ -0,0 +1,11 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+html = urlopen('http://www.pythonscraping.com/pages/page1.html')
+bs = BeautifulSoup(html.read(), 'html.parser')
+print(bs.h1)
+
+a = set()
+print(type(a))
+
+print("Hello Team")
